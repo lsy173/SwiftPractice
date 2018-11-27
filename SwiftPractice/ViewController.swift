d97aa8f9db8b3ed331c32ef0316a7da46517e556
이수영 <isuyeong@isuyeong-ui-macbook-pro.local>
2018-11-28 00:42:38 +0900

    Initial Commit

---
+++ SwiftPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftPractice
+//
+//  Created by 이수영 on 28/11/2018.
+//  Copyright © 2018 이수영. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
